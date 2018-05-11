604516bd16671acf726fdf233106e3ac06058fa7
Diana Baetscher <d.baetscher@gmail.com>
2018-05-11 13:56:57 -0700

    small additions for nsf data analysis.

---
+++ R-main/02-compile-and-tidy-nsf-gby-data.R
@@ -0,0 +1,137 @@
+library(tidyverse)
+library(readxl)
+library(stringr)
+library(lubridate)
+
+
+source("R/rockfish-funcs2.R")
+
+
+#### Call genos from the microhaplot RDS files ####
+# for non-target species in the gopher/black-and-yellow data set
+
+# this next step uses a little data frame that has the names and GTSeq run numbers
+# I got that file like this:
+# dsb:rds_files dianabaetscher$ pwd
+# /Users/dianabaetscher/Desktop/NOAA_grad/git-repos/rockfish-species-id/nsf_data/rds_files
+# dsb:rds_files dianabaetscher$ ls -l | awk 'BEGIN {print "gtseq_run", "file"} NR >1 && !/posinfo/ {num = $NF; gsub(/gtseq/, "", num); gsub(/[._].*$/, "", num);  print num, $NF}' > ../../nsf_data/rds-file-list.txt 
+
+# get the names of the files
+fdf <- read.table("nsf_data/rds-file-list.txt", stringsAsFactors = FALSE, header = TRUE) %>%
+  tbl_df()
+dir <- "nsf_data/rds_files"
+
+
+# cycle over them, read them and add the gtseq_run column on each.
+# at the end, bind them together.
+genos_long <- lapply(1:nrow(fdf), function(i) {
+  message("Working on ", fdf$file[i])
+  call_genos_from_haplotRDS(path = file.path(dir, fdf$file[i])) %>%
+    mutate(gtseq_run = fdf$gtseq_run[i]) %>%
+    select(gtseq_run, everything())
+}) %>%
+  bind_rows()
+
+# need to change the character GTseq to gtseq to match the sample sheet tibble
+v1 <- genos_long %>%
+  mutate(gtseq_run = replace(gtseq_run, str_detect(gtseq_run, "GTseq21"), "gtseq21"))
+v2 <- v1 %>%
+  mutate(gtseq_run = replace(gtseq_run, str_detect(gtseq_run, "GTseq43"), "gtseq43"))
+v3 <- v2 %>%
+  mutate(gtseq_run = replace(gtseq_run, str_detect(gtseq_run, "GTseq44"), "gtseq44"))
+v4 <- v3 %>%
+  mutate(gtseq_run = replace(gtseq_run, str_detect(gtseq_run, "GTseq45"), "gtseq45"))
+v5 <- v4 %>%
+  mutate(gtseq_run = replace(gtseq_run, str_detect(gtseq_run, "GTseq47"), "gtseq47"))
+v6 <- v5 %>%
+  mutate(gtseq_run = replace(gtseq_run, str_detect(gtseq_run, "GTseq49"), "gtseq49"))
+v7 <- v6 %>%
+  mutate(gtseq_run = replace(gtseq_run, str_detect(gtseq_run, "GTseq50"), "gtseq50"))
+genos_long <- v7 %>%
+  mutate(gtseq_run = replace(gtseq_run, str_detect(gtseq_run, "GTseq54"), "gtseq54"))
+
+# we go ahead and save it in data/processed, with xz compression
+saveRDS(genos_long, file = "nsf_data/processed/called_genos.rds", compress = "xz")
+
+
+#### Read the sample sheets from the Excel files ####
+
+# Same drill here.  First we make a file that holds a data frame of file names:
+# dsb:sample_sheets dianabaetscher$ pwd
+# /Users/dianabaetscher/Desktop/NOAA_grad/git-repos/rockfish-species-id/nsf_data/sample_sheets
+# dsb:sample_sheets dianabaetscher$ ls -l | awk 'BEGIN {print "gtseq_run", "file"} NR > 1 {num = $NF; gsub(/GTseq/, "", num); gsub(/_.*$/, "", num);  print num, $NF}' > ../../nsf_data/sample-sheet-file-list.txt
+
+
+fdf <- read.table("nsf_data/sample-sheet-file-list.txt", stringsAsFactors = FALSE, header = TRUE) %>%
+  tbl_df()
+dir <- "nsf_data/sample_sheets"
+
+sample_sheets <- lapply(1:nrow(fdf), function(i) {
+  message("Working on ", fdf$file[i])
+  read_excel(path = file.path(dir, fdf$file[i]), sheet = "sample_sheet", skip = 21) %>%
+    filter(!is.na(Sample_Plate)) %>%  # read_excel sometimes reads a lot of blank lines...
+    tidyr::separate(Sample_Plate, into = c("NMFS_DNA_ID", "ssBOX_ID", "ssBOX_POSITION")) %>%
+    mutate(id = str_replace(Sample_ID, "satrovirens_0*", "s")) %>%
+    mutate(gtseq_run = fdf$gtseq_run[i]) %>%
+    select(gtseq_run, id, everything())
+}) %>%
+  bind_rows()
+
+# 
+# # save that.
+# 
+saveRDS(sample_sheets, "nsf_data/processed/sample-sheet-tibble.rds", compress = "xz")
+# 
+
+
+
+#### And finally, let's get the meta-data read in and cleaned up (if it needs it) ####
+# meta1 <- read_csv("data/nsf-rockfish-metadata.csv") %>%
+#   select(-`Marine::NMFS_DNA_ID`) %>%
+#   mutate(BATCH_ID = as.character(BATCH_ID),
+#          WEIGHT = as.numeric(WEIGHT)) # get rid of the duplicated ID column
+# 
+# # when we read that in, we lose the "None."s in the LEFTOVER_SAMPLE fields.  That 
+# # is OK for now.  
+# 
+# # that was the majority of the meta data, but there are two other sources,
+# # as we have some old SMURF data (plus a few other rockfish that are in that
+# # metadata set) and also fish from the multiple-paternity study with Sue Sogard,
+# # so we are going to get those meta data and bind them on there as well.
+# more_meta <- read_delim("data/more-rockfish-metadata.txt", delim = "\t") %>%
+#   select(-HATCHERY_MARK, -LANDFALL_PORT, -CRUISE, -HAUL)  %>%  # blow away a few of these columns that we don't need 
+#   # so that the columns are congruent with meta
+#   select(-NMFS_DNA_ID_1)  # remove that duplicated ID column here too
+# 
+# names(meta1) <- str_replace(names(meta1), "^Marine::", "")  # get the "Marine::" out of some of the column names
+# 
+# meta <- bind_rows(meta1, more_meta) %>%
+#   mutate(COLLECTION_DATE = mdy(COLLECTION_DATE),
+#          PICK_DATE = mdy(PICK_DATE))
+# 
+# saveRDS(meta, "data/processed/meta-data-tibble.rds", compress = "xz")
+
+# for re-doing this analysis, I just read in the existing sample sheet tibble and meta data tibble
+#meta <- readRDS("data/processed/meta-data-tibble.rds")
+#sample_sheets <- readRDS("data/processed/sample-sheet-tibble.rds")
+
+
+#### In the end, let us get a data frame that includes genotypes for all the individuals  ####
+# and which explicity has NAs in places where data are missing, and also 
+# has the NMFS_DNA_ID on there
+genos_long_explicit_NAs <- sample_sheets %>%
+  select(gtseq_run, id, NMFS_DNA_ID) %>%
+  unite(col = gid, sep = "_", gtseq_run, id, NMFS_DNA_ID) %>%
+  select(gid) %>%
+  unlist() %>%
+  unname() %>%
+  expand.grid(gid = ., locus = unique(genos_long$locus), gene_copy = 1:2, stringsAsFactors = FALSE) %>%
+  tbl_df() %>% 
+  separate(gid, into = c("gtseq_run", "id", "NMFS_DNA_ID"), convert = TRUE) %>%
+  left_join(., genos_long) %>%
+  arrange(gtseq_run, id, locus, gene_copy)
+
+
+# and then save that
+saveRDS(genos_long_explicit_NAs, file = "nsf_data/processed/called_genos_na_explicit.rds", compress = "xz")
+
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 species identification in rockfishes
 ================
-10 March, 2018
+11 May, 2018
 
 -   [Preliminaries](#preliminaries)
 -   [Data files](#data-files)
@@ -9,7 +9,9 @@ species identification in rockfishes
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 This is an Rstudio project with data and analyses for species identification work that arose from the NSF rockfish dispersal project. Hayley helped expand the number of species sequenced, and I am creating a more reproducible and streamlined process here.
 
-We use amplicon-sequencing on our MiSeq instrument to generate genotype data for samples. Using these samples, we created a vcf file that contains variant sites from 49 species of rockfishes, mostly from the Northeast Pacific. These data correspond to GT-seq runs 11, 12, 19, 48, 54, 55, and 62 (maybe - need to check this with Hayley) and is located in this directory on Megabox. `/home/biopipe/Genetics_Lab_Data/GTseq/sebastes09282017/map/seb_SppID_all96loci_sorted_10022017.vcf`
+We use amplicon-sequencing on our MiSeq instrument to generate genotype data for samples. Using these samples, we created a vcf file that contains variant sites from 49 species of rockfishes, mostly from the Northeast Pacific. These data correspond to GT-seq runs 11, 12, 19, 48, 54, 55, and 62 and is located in this directory on Megabox.
+
+`/home/biopipe/Genetics_Lab_Data/GTseq/sebastes09282017/map/seb_SppID_all96loci_sorted_10022017.vcf`
 
 I have modified Hayley's original workflow to adapt it to an R project and make it more reproducible: this is still a work in progress.
 
@@ -18,24 +20,36 @@ Preliminaries
 
 In order to perform Sebastes species ID...
 
-1.  Process the GTseq run with the samples you wish to identify using the combined Sebastes vcf in MICROHAPLOT. The most current vcf can be found at: /home/biopipe/Genetics\_Lab\_Data/GTseq/sebastes09282017/map/seb\_SppID\_all96loci\_sorted\_10022017.vcf
+1.  Process the GTseq run with the samples you wish to identify using the combined Sebastes vcf in MICROHAPLOT. The most current vcf can be found at:
 
-2.  Copy rds files from HaPLOType (the minimum read depth and min allelic ratio filters are applied in `rockfish-funcs2.R` when reading the rds files into this R project)
+/home/biopipe/Genetics\_Lab\_Data/GTseq/sebastes09282017/map/seb\_SppID\_all96loci\_sorted\_10022017.vcf
+
+1.  Copy rds files from HaPLOType (the minimum read depth and min allelic ratio filters are applied in `rockfish-funcs2.R` when reading the rds files into this R project)
 
 Data files
 ----------
 
 1.  rds files created by the R software program MICROHAPLOT should be placed in the `data/rds_files` directory, with instructions for reading in genotypes data in `R-main/01-compile-and-tidy-data.R`.
 
-2.  The Illumina MiSeq sample sheets associated with these genotype data should be placed in `data/sample_sheets`. This is the means by which sample ID numbers from the sequencing runs can be connected back to NMFS DNA ID as recorded in meta data and the repository.
+2.  The MiSeq sample sheets associated with these genotype data should be placed in `data/sample_sheets`. This is the means by which sample ID numbers from the sequencing runs can be connected back to NMFS DNA ID as recorded in meta data and the repository.
 
 Data filtering
 --------------
 
 1.  Remove the 6 loci that were most prone to missing data and/or often have more than 3 haplotypes passing filter across individuals:
 
-tag\_id\_1166 (often &gt;2 haplotypes) tag\_id\_934 (missing data and sometimes &gt;2 haplotypes) tag\_id\_2513 (appears to be some sort of repetitive element for some species outside of KGBC) tag\_id\_1871 (failed in many individuals) tag\_id\_1399 (failed in many individuals) tag\_id\_914 (failed in many individuals)
+-   tag\_id\_1166 (often &gt;2 haplotypes)
 
-1.  The missing data threshold of 15 loci doesn't make sense given the dramatic ascertainment bias from kelp rockfish to less closely related species across phylogenetic distance. For now, I am keeping all individuals in the baseline.
+-   tag\_id\_934 (missing data and sometimes &gt;2 haplotypes)
+
+-   tag\_id\_2513 (appears to be some sort of repetitive element for some species outside of KGBC)
+
+-   tag\_id\_1871 (failed in many individuals)
+
+-   tag\_id\_1399 (failed in many individuals)
+
+-   tag\_id\_914 (failed in many individuals)
+
+1.  Typically we institute a missing data threshold of ~10% for excluding samples, but such a criterion doesn't make sense given the dramatic ascertainment bias from kelp rockfish to less closely related species across phylogenetic distance. For now, I am keeping all individuals in the baseline.
 
 2.  An example of species identification of a mixture of samples using rubias is outlined in `02-assign-mixture-w-rubias.Rmd`.
